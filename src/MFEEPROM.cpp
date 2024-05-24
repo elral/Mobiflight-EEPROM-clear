75fc2ab58ed7819c59a8543311c1ca87957630b6
elral <3263285+elral@users.noreply.github.com>
2024-05-24 14:33:15 +0200

    use buffered eeprom functions for STM32

--- src/MFEEPROM.cpp
+++ src/MFEEPROM.cpp
@@ -17,6 +17,9 @@ void MFEEPROM::init(void)
 #if defined(ARDUINO_ARCH_RP2040)
     EEPROM.begin(EEPROM_SIZE);
 #endif
+#if defined(ARDUINO_ARCH_STM32)
+    eeprom_buffer_fill();
+#endif
     _eepromLength = EEPROM.length();
 }
 
@@ -38,10 +41,17 @@ bool MFEEPROM::write_block(uint16_t adr, char data[], uint16_t len)
 {
     if (adr + len > _eepromLength) return false;
     for (uint16_t i = 0; i < len; i++) {
+#if defined(ARDUINO_ARCH_STM32)
+        eeprom_buffered_write_byte(adr, data);
+#else
         EEPROM.put(adr + i, data[i]);
+#endif
     }
 #if defined(ARDUINO_ARCH_RP2040)
     EEPROM.commit();
+#endif
+#if defined(ARDUINO_ARCH_STM32)
+    eeprom_buffer_flush();
 #endif
     return true;
 }
@@ -49,15 +59,26 @@ bool MFEEPROM::write_block(uint16_t adr, char data[], uint16_t len)
 char MFEEPROM::read_char(uint16_t adr)
 {
     if (adr >= _eepromLength) return 0;
+#if defined(ARDUINO_ARCH_STM32)
+            return eeprom_buffered_read_byte(adr);
+#else
     return EEPROM.read(adr);
+#endif
 }
 
 bool MFEEPROM::write_byte(uint16_t adr, char data)
 {
     if (adr >= _eepromLength) return false;
+#if defined(ARDUINO_ARCH_STM32)
+    eeprom_buffered_write_byte(adr, data);
+#else
     EEPROM.put(adr, data);
+#endif
 #if defined(ARDUINO_ARCH_RP2040)
     EEPROM.commit();
+#endif
+#if defined(ARDUINO_ARCH_STM32)
+    eeprom_buffer_flush();
 #endif
     return true;
 }
